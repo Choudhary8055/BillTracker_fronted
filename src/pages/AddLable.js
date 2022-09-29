b8e47f11dd980499f7cd0b99ee1a9cb068b7b811
Choudhary8055 <rahulchoudhary200599@gmail.com>
2022-09-29 12:22:14 +0530

    new

---
+++ src/pages/AddLable.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function AddLable() {
+    const [lable, setLable] = useState("");
+    const [amount, setAmount]=useState("");
+    const navigate = useNavigate();
+    const storeData = async ()=>{
+        const data = {
+            lable,
+             amount
+        };
+       
+		const url = 'http://localhost:3500/addlable';
+
+		let result = await axios.post(url, data);
+    navigate('/');
+}
+
+  return (
+    <>
+     <div className='product'>
+        Lable:
+      <input placeholder='Enter the label name' value={lable} onChange = {(e)=> setLable(e.target.value)} className="inputBox"/>
+      Amount:
+      <input placeholder='Enter the label amount' value={amount} onChange = {(e)=> setAmount(e.target.value)} className="inputBox"/>
+      <button onClick={storeData} className="appButton">Save</button>
+      </div>
+    
+    </>
+   
+  )
+}
+
+export default AddLable;
